91fdb3d473a44cfeef075a2a23b1e7d314c4d0ac
shadydima <59603646+shadydima@users.noreply.github.com>
2020-01-07 18:52:51 +0300

    Update zagadki.py

--- zagadki.py
+++ zagadki.py
@@ -1,11 +1,400 @@
-zagadki= [  {"text" : "сто одежек и все без застежек","answer" : "лук"} , {"text" : "На зелёной хрупкой ножке вырос шарик у дорожки","answer" : "одуванчик"},{"text" : "Мягкие лапки, а в лапках царапки","answer" : "кошка"}]
-def run(zagadki):
-    i=0
-    while(i<=2)
-        print("Первая загадка", zagadki[i]["text"])
-        print("Выведи ответ")
-        ans1=input()
-        if ans1==zagadki[i]["answer"] :
-            print("Ты отгадал!")
-        else:
-            print("Ты не отгадал")
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>DoYoga &mdash; Website Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    
+    
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div id="overlayer"></div>
+  <div class="loader">
+    <div class="spinner-border text-primary" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+    
+    
+    <div class="site-navbar-wrap">
+      <div class="site-navbar-top">
+        <div class="container py-3">
+          <div class="row align-items-center">
+            <div class="col-6">
+              <div class="d-flex mr-auto">
+                <a href="#" class="d-flex align-items-center mr-4">
+                  <span class="icon-envelope mr-2"></span>
+                  <span class="d-none d-md-inline-block">dashadymov@yandex.ru</span>
+                </a>
+                <a href="#" class="d-flex align-items-center mr-auto">
+                  <span class="icon-phone mr-2"></span>
+                  <span class="d-none d-md-inline-block">+7 910 885 34 32</span>
+                </a>
+              </div>
+            </div>
+            <div class="col-6 text-right">
+              <div class="mr-auto">
+                <a href="#" class="p-2 pl-0"><span class="icon-twitter"></span></a>
+                <a href="#" class="p-2 pl-0"><span class="icon-facebook"></span></a>
+                <a href="#" class="p-2 pl-0"><span class="icon-linkedin"></span></a>
+                <a href="#" class="p-2 pl-0"><span class="icon-instagram"></span></a>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-navbar site-navbar-target js-sticky-header">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-2">
+              <h1 class="my-0 site-logo"><a href="index.html">🏥</a></h1>
+            </div>
+            <div class="col-10">
+              <nav class="site-navigation text-right" role="navigation">
+                <div class="container">
+                  <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+                  <ul class="site-menu main-menu js-clone-nav d-none d-lg-block">
+                    <li><a href="#home-section" class="nav-link">Главная</a></li>
+                    <li><a href="#classes-section" class="nav-link">Голова</a></li>
+                    <li><a href="#classes-sectio" class="nav-link">Туловище</a></li>
+                    </li>
+                    <li><a href="#about-section" class="nav-link">Конечности</a></li>
+                    <li><a href="#gallery" class="nav-link">Галерея</a></li>
+                    <li><a href="#contact-section" class="nav-link">Контакты</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="site-blocks-cover overlay" style="background-image: url(images/doyoga_hero_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5"id="home-section">
+      <div class="container">
+        <div class="row align-items-center text-center justify-content-center">
+          <div class="col-md-8">
+           
+            <h1 class="font-weight-bold">HEALTHCHECK</h1>
+            <p class="sub-text mb-4 d-block">Ваш персональный проводник к здоровому образу жизни</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    
+    <section class="site-section" id="classes-section">
+
+      <div class="clearfix mb-5 pb-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-7 text-left heading-wrap">
+              <h2 class="mt-0">Голова</h2>
+              <p>Чаще всего с головной болью можно справиться с помощью нестероидных противовоспалительных средств (ибупрофена, аспирина, напроксена и др.), если, конечно, у вас нет противопоказаний. Самый распространенный вид головной боли — головная боль напряжения — так и лечится. При втором по частоте виде головной боли — мигрени — помогут не только аспирин с ибупрофеном, но и триптаны (средства, сужающие сосуды и блокирующие работу нервных волокон, проводящих болевой сигнал; эти препараты назначает врач)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <section class="site-section" id="classes-sectio">
+
+      <div class="clearfix mb-5 pb-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-7 text-left heading-wrap">
+              <h2 class="mt-0">Туловище</h2>
+              <p>Боль в животе — симптом, который лучше не оставлять без внимания. Причиной такой боли может стать любой орган брюшной полости. Но в каких случаях стоит беспокоиться и бежать к врачу.Боли в животе (мед.: абдоминальные боли) или спазмы живота появляются хоть один раз у каждого человека. Чаще всего, они через некоторое время проходят. Однако в некоторых случаях боли бывают продолжительные и имеют хронический характер.
+Причины болей  весьма разнообразны – большинство из них относительно безобидны. Но есть и серьезные заболевания, которые проявляются через боли в животе. Важно распознать случаи, когда нужно срочно обратиться к врачу.
+Боль  может быть вызвана изменениями  внутрикишечного давления в тонкой и толстой кишках, растяжении просвета или сокращении мышечной оболочки, а также нарушениями кровоснабжения.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+    <section class="site-section" id="about-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 mr-auto">
+            <div class="text-left heading-wrap mb-5">
+              <h2 class="mt-0 mb-5">Конечности</h2>
+              <p>Независимо от того, какое заболевание стало причиной возникновения артралгии, врачи обязательно назначают медикаментозное лечение, в первую очередь — нестероидные противовоспалительные средства (НПВС), если их приему не мешают нарушения свертываемости крови, острые заболевания ЖКТ (например, язва). НПВС снимают отек, гиперемию, понижают температуру и интенсивность развития воспалительного процесса. К таким препаратам относятся «Ибупрофен», «Диклофенак», «Мелоксикам», «Аспирин» и др. Кроме НПВС могут быть назначены сосудорасширяющие препараты («Актовегин», «Эуфиллин», «Липоевая кислота») для ускорения кровотока в очаге патологии и стимуляции процесса восстановления.Хирургические методы лечения артралгии — это серьезное вмешательство в работу организма и назначается оно в крайних случаях. К таким методам относится пункция сустава, в ходе которой врач с помощью специальной иглы удаляет воспалительную жидкость или вводит медикаменты в суставную полость.К основным реабилитационным методам лечения боли в суставах относятся кинезитерапия (лечение движением), массаж, механотерапия, тракционное лечение, рефлексотерапия, трудотерапия, психотерапия.
+К этому типу относится ударно-волновая терапия, основанная на эффекте кавитации, при котором динамические импульсы стимулируют кровообращение, усиливают синтез костной ткани, разрушают отложения солей.</p>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="site-section" id="gallery">
+      <div class="container">
+
+          <div class="col-12 heading-wrap text-center mb-5">
+            <h2 class="mb-5"> ГАЛЕРЕЯ ЗДОРОВЬЯ</h2>
+          </div>
+
+
+        <div id="posts" class="row no-gutter">
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/doyoga_img_1.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/IMG_9435.jpg">
+            </a>
+          </div>
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/doyoga_img_2.jpg" class="item-wrap fancybox">
+              <span class="icon-search1"></span>
+              <img class="img-fluid" src="images/IMG_9437.jpg">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/doyoga_img_2.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/IMG_9440.jpg">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/doyoga_about_3.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/IMG_9438jpg">
+            </a>
+          </div>
+
+
+
+          <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/doyoga_about_2.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/doyoga_about_2.jpg">
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <section class="site-section element-animate" id="contact-section">
+      <div class="mb-5">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12 text-center heading-wrap">
+              <h2>Связаться с нами</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <form action="#" method="post">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <label for="name">Имя</label>
+                  <input type="text" id="name" class="form-control ">
+                </div>
+                <div class="col-md-6 form-group">
+                  <label for="phone">Телефон</label>
+                  <input type="text" id="phone" class="form-control ">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label for="email">Эл. адрес</label>
+                  <input type="email" id="email" class="form-control ">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label for="message">Написать письмо</label>
+                  <textarea name="message" id="message" class="form-control " cols="30" rows="8"></textarea>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="submit" value="Отправить" class="btn btn-primary">
+                </div>
+              </div>
+            </form>
+          </div>
+          
+          <div class="col-lg-6 pl-2 pl-lg-5">
+
+            <div class="col-md-8 mx-auto contact-form-contact-info">
+              <h4 class="mb-5">Контакты</h4>
+                <p class="d-flex">
+                  <span class="ion-ios-location icon mr-5"></span>
+                  <span>32 ул. Смирнова,GarTown, пентхаус, Москва Сити, Россия</span>
+                </p>
+
+                <p class="d-flex">
+                  <span class="ion-ios-telephone icon mr-5"></span>
+                  <span>+7 910 885 34 32</span>
+                </p>
+
+                <p class="d-flex">
+                  <span class="ion-android-mail icon mr-5"></span>
+                  <span>dashadymov@yanedx.ru</span>
+                </p>
+              </div>
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 mb-5 mb-lg-0">
+            <div class="row mb-5">
+              <div class="col-12">
+                <h3 class="footer-heading mb-4">Немного про нас</h3>
+                <p>Мы команда ребят из Нижнего Новгорода и Нефтекамска. Нас всего пять человек, но мы хотим создать собственный продукт, который поможет многим. Наш проект – сайт помогающий людям найти возможное заболевание по симптомам и узнать как можно его вылечить. Зачем нам это? Проблема сохранения здоровья постоянно интересовала нас .
+                  В наш век она особа актуальна.</p>
+              </div>
+            </div>
+            
+
+            
+          </div>
+          <div class="col-lg-4">
+           
+            <div class="row mb-5">
+              <div class="col-md-12">
+                <h3 class="footer-heading mb-4">Быстрые ссылки</h3>
+              </div>
+              <div class="col-md-6 col-lg-6">
+                <ul class="list-unstyled">
+                  <li><a href="#">Дом</a></li>
+                  <li><a href="#">Сервисы</a></li>
+                  <li><a href="#">События</a></li>
+                  <li><a href="#">Команда</a></li>
+                </ul>
+              </div>
+              <div class="col-md-6 col-lg-6">
+                <ul class="list-unstyled">
+                  <li><a href="#">Самсонова Анастасия</a></li>
+                  <li><a href="#">Кирилл Умилин</a></li>
+                  <li><a href="#">Гарайшин Эрнест</a></li>
+                  <li><a href="#">Шадымов Дмитрий</a></li>
+                  <li><a href="#">Каримов Данил</a></li>
+                </ul>
+              </div>
+            </div>
+            
+          </div>
+          
+
+          <div class="col-lg-4 mb-5 mb-lg-0">
+
+            <div class="mb-5">
+              <h3 class="footer-heading mb-2">Подписаться на рассылку</h3>
+              <p>Нас всего пять человек, но мы хотим создать собственный продукт, который поможет многим.</p>
+
+              <form action="#" method="post" class="form-subscribe">
+                <div class="input-group mb-3">
+                  <input type="text" class="form-control border-white text-white bg-transparent" placeholder="Эл. почта" aria-label="Эл. почта" aria-describedby="button-addon2">
+                  <div class="input-group-append">
+                    <button class="btn btn-primary" type="button" id="button-addon2">Отправить</button>
+                  </div>
+                </div>
+              </form>
+
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <h3 class="footer-heading mb-4">Соц. сети</h3>
+
+                <div>
+                  <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                  <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                  <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                  <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/main.js"></script>
+
+  </body>
+</html>
